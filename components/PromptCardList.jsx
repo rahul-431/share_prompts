6c818e9289e44656c70da33ee111184746e4b2e0
Rahul Mijar <rahulmijar0243@gmail.com>
2024-09-01 13:27:50 +0545

    refactor: gobal.css

--- components/PromptCardList.jsx
+++ components/PromptCardList.jsx
@@ -4,7 +4,7 @@ const PromptCardList = ({ handleTagClick, data }) => {
   return (
     <div className="my-8 flex flex-col w-full gap-4">
       <h1 className="text-xl font-normal text-left">Latest shared prompts</h1>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 justify-around gap-6 w-full">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-around gap-6 w-full">
         {data.map((post) => (
           <PromptCard
             key={post._id}
--- components/ToolCardList.jsx
+++ components/ToolCardList.jsx
@@ -4,7 +4,7 @@ const ToolCardList = ({ handleTagClick, data }) => {
   return (
     <div className="my-8 flex flex-col w-full gap-4" id="tools">
       <h1 className="text-xl font-normal text-left">Latest shared AI Tools</h1>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 justify-around gap-6 w-full">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-around gap-6 w-full">
         {data.map((post) => (
           <ToolCard
             key={post._id}
